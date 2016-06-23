34d68eee5fa433db380b049167a34bf79e00d54b
wchang28 <wchang28@hotmail.com>
2016-06-23 15:22:47 -0400

    jkgsfhgsfhg

--- ts/client-app/index.ts
+++ ts/client-app/index.ts
@@ -1,7 +1,16 @@
 import * as express from 'express';
 import * as core from 'express-serve-static-core';
+import {IUser} from '../messaging';
 
 let router = express.Router();
 
-//console.log('I am here');
+function getUser(req: express.Request): IUser {
+    let user:IUser = req["user"];
+    return user;
+}
+
+router.get('/submit_job', (req: express.Request, res: express.Response) => {
+    res.json({});
+});
+
 export {router as Router};
