c154bb584ccbbc7f55e3fff81ec04fa593fb7f2b
wchang28 <wchang28@hotmail.com>
2016-07-23 08:00:33 -0400

    bwtebst

--- ts/errors.ts
+++ ts/errors.ts
@@ -1,12 +1,10 @@
-export interface IError {
-    error: any;
-    error_description?: string;
-}
+import * as oauth2 from 'oauth2';
+export {IError} from 'oauth2';
 
-let err_not_authorized: IError = {error: 'not_authorized', error_description: 'not authorized'};
-let err_bad_node_id: IError = {error: 'bad_node_id', error_description: 'invalid node id'};
-let err_invalid_node: IError = {error: 'invalid_node', error_description: 'not a valid node'};
-let err_bad_job_id: IError = {error: 'bad_job_id', error_description: 'invalid job id'};
+let err_not_authorized = oauth2.errors.not_authorized;
+let err_bad_node_id: oauth2.IError = {error: 'bad_node_id', error_description: 'invalid node id'};
+let err_invalid_node: oauth2.IError = {error: 'invalid_node', error_description: 'not a valid node'};
+let err_bad_job_id: oauth2.IError = {error: 'bad_job_id', error_description: 'invalid job id'};
 
 export {err_not_authorized as not_authorized};
 export {err_bad_node_id as bad_node_id};
