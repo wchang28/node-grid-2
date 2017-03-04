cd66360f74d129fc5e0068ba25c573cafafbb77f
wchang28 <wchang28@hotmail.com>
2017-03-03 20:10:10 -0500

    ergeg

--- ts/test/clientTest.ts
+++ ts/test/clientTest.ts
@@ -5,12 +5,12 @@ import {run as runSomeTestJob} from './runSomeTestJob';
 
 let username = process.argv[2];
 if (!username) {
-    console.error('!!! musr enter username');
+    console.error('!!! must enter username');
     process.exit(1);
 }
 let password = process.argv[3];
 if (!password) {
-    console.error('!!! musr enter password');
+    console.error('!!! must enter password');
     process.exit(1);
 }
 
