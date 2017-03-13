f626df66e86ef82db44f694e731ea4e5bc78dcb8
wchang28 <wchang28@hotmail.com>
2017-03-12 21:50:13 -0400

    fdhjabhjgva

--- ts/services/autoscaler/index.ts
+++ ts/services/autoscaler/index.ts
@@ -22,3 +22,60 @@ router.post('/launch_new_workers', (req: express.Request, res: express.Response)
 router.post('/terminate_workers', (req: express.Request, res: express.Response) => {
     res.jsonp(getAutoScaler(req).terminateWorkers(req.body));
 });
+
+router.get('/is_enabled', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).Enabled);
+});
+
+router.post('/enable', (req: express.Request, res: express.Response) => {
+    getAutoScaler(req).Enabled = true;
+    res.jsonp({});
+});
+
+router.post('/disable', (req: express.Request, res: express.Response) => {
+    getAutoScaler(req).Enabled = false;
+    res.jsonp({});
+});
+
+router.get('/has_max_workers_cap', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).HasMaxWorkersCap);
+});
+
+router.get('/has_min_workers_cap', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).HasMinWorkersCap);
+});
+
+router.get('/get_max_workers_cap', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).MaxWorkersCap);
+});
+
+router.post('/set_max_workers_cap', (req: express.Request, res: express.Response) => {
+    getAutoScaler(req).MaxWorkersCap = req.body;
+    res.jsonp(getAutoScaler(req).MaxWorkersCap);
+});
+
+router.get('/get_min_workers_cap', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).MinWorkersCap);
+});
+
+router.post('/set_min_workers_cap', (req: express.Request, res: express.Response) => {
+    getAutoScaler(req).MinWorkersCap = req.body;
+    res.jsonp(getAutoScaler(req).MinWorkersCap);
+});
+
+router.get('/get_launching_workers', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).LaunchingWorkers);
+});
+
+router.get('/', (req: express.Request, res: express.Response) => {
+    res.jsonp(getAutoScaler(req).toJSON());
+});
+
+router.get('/get_impl_config_url', (req: express.Request, res: express.Response) => {
+    getAutoScaler(req).ImplementationConfigUrl
+    .then((url: string) => {
+        res.jsonp(url);
+    }).catch((err: any) => {
+        res.status(400).json(err);
+    });
+});
