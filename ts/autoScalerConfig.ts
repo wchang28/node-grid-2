7d4c64ed52cc73b4bccce0904d291ece6c12e82d
wchang28 <wchang28@hotmail.com>
2017-03-14 12:15:11 -0400

    gfndgh

--- ts/autoScalerConfig.ts
+++ ts/autoScalerConfig.ts
@@ -1,18 +1,4 @@
 import {Options as AutoScalerOptions} from 'grid-autoscaler';
-/*
-import {IAutoScalerImplementation} from 'autoscalable-grid';
-import * as express from 'express';
-import * as core from 'express-serve-static-core';
-
-export type AutoScalerImplementationFactory = (options?: any) => Promise<IAutoScalerImplementation>;
-export type GetAutoScalerImplementationProc = (req: express.Request) => Promise<IAutoScalerImplementation>;
-export type AutoScalerImplementationRouterFactory = (getImplProc: GetAutoScalerImplementationProc) => Promise<express.Router>;
-
-export interface AutoScalerImplementationPackageExport {
-    factory: AutoScalerImplementationFactory;
-    routerFactory?: AutoScalerImplementationRouterFactory;
-}
-*/
 
 export interface AutoScalerImplementationConfig {
     factoryPackagePath: string;
