c364c8a003792627a25764d5d857a410140bd876
wchang28 <wchang28@hotmail.com>
2017-03-13 20:18:08 -0400

    ghdnjg

--- ts/test/autoScalerImpl/index.ts
+++ ts/test/autoScalerImpl/index.ts
@@ -1,4 +1,6 @@
 import {IWorker, IAutoScalerImplementation, IAutoScalableState, WorkerKey, IWorkersLaunchRequest} from 'autoscalable-grid';
+import * as express from 'express';
+import * as core from 'express-serve-static-core';
 
 export interface Options {
     CPUsPerWorker: number;
@@ -33,4 +35,41 @@ class Implementation implements IAutoScalerImplementation {
 
 export function factory(options: Options) : Promise<IAutoScalerImplementation> {
     return Promise.resolve<IAutoScalerImplementation>(new Implementation(options));
+}
+
+export type GetAutoScalerImplementationProc = (req: express.Request) => Promise<IAutoScalerImplementation>;
+
+function getImplementation(req: express.Request, getImplProc: GetAutoScalerImplementationProc) : Promise<Implementation> {
+    return new Promise<Implementation>((resolve: (value: Implementation) => void, reject: (err: any) => void) => {
+        getImplProc(req)
+        .then((impl: IAutoScalerImplementation) => {
+            let o: any = impl;
+            resolve(o);
+        }).catch((err: any) => {
+            reject(err);
+        });
+    });
+}
+
+type Handler = (impl: Implementation) => Promise<any>;
+
+function getRequestHandler(getImplProc: GetAutoScalerImplementationProc, handler: Handler) : express.RequestHandler {
+    return (req: express.Request, res: express.Response) => {
+        getImplementation(req, getImplProc)
+        .then((impl: Implementation) => {
+            return handler(impl)
+        }).then((ret: any) => {
+            res.jsonp(ret);
+        }).catch((err: any) => {
+            res.status(400).json(err);
+        })
+    }
+}
+
+export function routerFactory(getImplProc: GetAutoScalerImplementationProc) : Promise<express.Router> {
+    let router = express.Router();
+    router.get('/config_url', getRequestHandler(getImplProc, (impl: Implementation) => {
+        return impl.getConfigUrl();
+    }));
+    return Promise.resolve<express.Router>(router);
 }
