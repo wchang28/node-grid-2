72c0824e387a16e767ec0626b2078398eda37f6a
wchang28 <wchang28@hotmail.com>
2017-03-21 09:43:11 -0400

    brtbt

--- ts/ui/autoScaler.tsx
+++ ts/ui/autoScaler.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 import {Utils, IGridAutoScalerJSON, AutoScalerImplementationInfo, IMessageClient, GridMessage, IGridAutoScaler} from 'grid-client-core';
 
-interface IAutoScalerProps {
+export interface IAutoScalerProps {
     autoScalerAvailable: boolean
     gridAutoScaler: IGridAutoScaler
     msgClient: IMessageClient<GridMessage>;
 }
 
-interface IAutoScalerState {
+export interface IAutoScalerState {
     sub_id?: string;
     autoScalerJSON?: IGridAutoScalerJSON;
     autoScalerImplementationInfo?: AutoScalerImplementationInfo;
 }
 
-export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerState> {
+export class AutoScalerUI extends React.Component<IAutoScalerProps, IAutoScalerState> {
     constructor(props:IAutoScalerProps) {
         super(props);
         this.state = {
@@ -22,7 +22,12 @@ export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerSta
             ,autoScalerImplementationInfo: null
         };
     }
-    protected get msgClient(): IMessageClient<GridMessage> {return this.props.msgClient;}
+    protected handleMessages(gMsg: GridMessage) : void {
+        if (gMsg.type === 'autoscaler-changed') {
+            this.getAutoScalerJSON();
+        }
+    }
+    protected get MsgClient(): IMessageClient<GridMessage> {return this.props.msgClient;}
     protected get GridAutoScaler(): IGridAutoScaler {return this.props.gridAutoScaler;}
     private getAutoScalerJSON() {
         this.GridAutoScaler.getJSON()
@@ -33,7 +38,7 @@ export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerSta
         });
     }
     private getAutoScalerImplementationInfo() {
-        this.session.GridAutoScaler.getImplementationInfo()
+        this.GridAutoScaler.getImplementationInfo()
         .then((autoScalerImplementationInfo: AutoScalerImplementationInfo) => {
             this.setState({autoScalerImplementationInfo})
         }).catch((err: any) => {
@@ -46,7 +51,7 @@ export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerSta
         if (this.props.autoScalerAvailable) {
             this.getAutoScalerJSON();
             this.getAutoScalerImplementationInfo();
-            this.msgClient.subscribe(Utils.getAutoScalerTopic(), this.handleMessages.bind(this), {})
+            this.MsgClient.subscribe(Utils.getAutoScalerTopic(), this.handleMessages.bind(this), {})
             .then((sub_id: string) => {
                 console.log('autoscaler topic subscribed, sub_id=' + sub_id + " :-)");
                 this.setState({sub_id});
@@ -59,7 +64,7 @@ export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerSta
         console.log('HomeContent.componentWillUnmount()');
         if (this.state.sub_id) {
             let sub_id = this.state.sub_id;
-            this.msgClient.unsubscribe(sub_id)
+            this.MsgClient.unsubscribe(sub_id)
             .then(() => {
                 console.log('successfully unsubscribed subscription ' + sub_id);
             }).catch((err: any) => {
@@ -67,6 +72,107 @@ export class AutoScaler extends React.Component<IAutoScalerProps, IAutoScalerSta
             });
         }
     }
+    private booleanString(val: boolean) : string {return (val ? "Yes": "No");}
+
+    private get AutoScalerAvailable(): boolean {return this.props.autoScalerAvailable};
+    private get AutoScalerJSON(): IGridAutoScalerJSON {return this.state.autoScalerJSON;}
+    private get AutoScalerImplInfo(): AutoScalerImplementationInfo {return this.state.autoScalerImplementationInfo;}
+    // TODO: check profile
+    private get AllowToChangeAutoScalerConfig() : boolean {
+        if (!this.AutoScalerAvailable)
+            return false;
+        else if (!this.AutoScalerJSON)
+            return false;
+        else
+            return true;
+    }
+    private get AutoScalerEnabledText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.booleanString(this.AutoScalerJSON.Enabled) : null) : "N/A");}
+    private get AutoScalerScalingText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? (this.AutoScalerJSON.ScalingUp ? "Scaling up...": "Idle") : null) : "N/A");}
+    private get AutoScalerMaxNodesText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.MaxWorkersCap.toString() : null) : "N/A");}
+    private get AutoScalerMinNodesText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.MinWorkersCap.toString() : null) : "N/A");}
+    private get AutoScalerNodeLaunchingTimeoutText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.LaunchingTimeoutMinutes.toString() + " min." : null) : "N/A");}
+    private get AutoScalerTerminateWorkerAfterMinutesIdleText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.TerminateWorkerAfterMinutesIdle.toString() + " min." : null) : "N/A");}
+    private get AutoScalerRampUpSpeedRatioText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.RampUpSpeedRatio.toString() : null) : "N/A");}
+    private get AutoScalerImplName() : string {return (this.AutoScalerAvailable ? (this.AutoScalerImplInfo ? this.AutoScalerImplInfo.Name : null) : "N/A");}
+    private get HasAutoScalerImplSetupUI() : boolean {return (this.AutoScalerAvailable ? (this.AutoScalerImplInfo ? this.AutoScalerImplInfo.HasSetupUI : false) : false);}
+    private get AutoScalerImplSetupUILinkEnabled() : boolean {return this.AllowToChangeAutoScalerConfig && this.HasAutoScalerImplSetupUI;}
+    private get AutoScalerImplSetupUIUrl() : string {return (this.HasAutoScalerImplSetupUI ? "autoscaler/implementation" : "#");}
+
+    private onAutoScalerEnableClick(e:any) {
+        if (this.state.autoScalerJSON) {
+            let p = (this.state.autoScalerJSON.Enabled ? this.GridAutoScaler.disable() : this.GridAutoScaler.enable());
+            p.then(() => {
+
+            }).catch((err: any) => {
+                console.error('!!! Unable to enable/disable auto-scaler: ' + JSON.stringify(err));
+            });
+        }
+    }
     render() {
+        return (
+            <div className="w3-card-4 w3-margin">
+                <div className="w3-container w3-blue">
+                    <h6>Auto-Scaler ({this.AutoScalerAvailable ? "Available" : "N/A"})</h6>
+                </div>
+                <div className="w3-container w3-white">
+                    <table className="w3-table w3-bordered w3-small">
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Value</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Enabled</td>
+                                <td>{this.AutoScalerEnabledText}</td>
+                                <td><button disabled={!this.AllowToChangeAutoScalerConfig} onClick={this.onAutoScalerEnableClick.bind(this)}>{!this.state.autoScalerJSON || this.state.autoScalerJSON.Enabled ? "Disable" : "Enable"}</button></td>
+                            </tr>
+                            <tr>
+                                <td>Scaling</td>
+                                <td>{this.AutoScalerScalingText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Max. # of nodes</td>
+                                <td>{this.AutoScalerMaxNodesText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Min. # of nodes</td>
+                                <td>{this.AutoScalerMinNodesText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Node launching timeout</td>
+                                <td>{this.AutoScalerNodeLaunchingTimeoutText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Terminate node after idle for </td>
+                                <td>{this.AutoScalerTerminateWorkerAfterMinutesIdleText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Ramp up speed ratio</td>
+                                <td>{this.AutoScalerRampUpSpeedRatioText}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Implementation Name</td>
+                                <td>{this.AutoScalerImplName}</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Additional config.</td>
+                                <td></td>
+                                <td><a disabled={!this.AutoScalerImplSetupUILinkEnabled} href={this.AutoScalerImplSetupUIUrl}>Click Here</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
     }
 }
--- ts/ui/homeContent.tsx
+++ ts/ui/homeContent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {IMessageClient, GridMessage, Utils, ISession, IGridUser, IDispatcherJSON, INodeItem, IDispControl, IQueueJSON, Times, IGridAutoScalerJSON, AutoScalerImplementationInfo} from 'grid-client-core';
+import {AutoScalerUI} from './autoScaler';
 
 export interface IHomeContentProps {
     msgClient: IMessageClient<GridMessage>;
@@ -11,20 +12,17 @@ export interface IHomeContentProps {
 
 export interface IHomeContentState {
     disp_sub_id?:string;
-    autoscaler_sub_id?:string;
     nodes?: INodeItem[];
     queue?:IQueueJSON;
     dispControl?: IDispControl;
     timer?: any;
     times?: Times;
-    autoScalerJSON?: IGridAutoScalerJSON;
-    autoScalerImplementationInfo?: AutoScalerImplementationInfo;
 }
 
 export class HomeContent extends React.Component<IHomeContentProps, IHomeContentState> {
     constructor(props:IHomeContentProps) {
         super(props);
-        this.state = {disp_sub_id: null, autoscaler_sub_id: null, timer: null, times: null, autoScalerJSON: null, autoScalerImplementationInfo: null};
+        this.state = {disp_sub_id: null, timer: null, times: null};
     }
     protected get msgClient(): IMessageClient<GridMessage> {return this.props.msgClient;}
     protected get session(): ISession {return this.props.session;}
@@ -41,8 +39,6 @@ export class HomeContent extends React.Component<IHomeContentProps, IHomeContent
             //console.log('receive <<queue-changed>>: ' + JSON.stringify(gMsg.content));
             let queue: IQueueJSON = gMsg.content;
             this.setState({queue: queue});
-        } else if (gMsg.type === 'autoscaler-changed') {
-            this.getAutoScalerJSON();
         }
     }
     private getDispatcherJSON() : void {
@@ -65,22 +61,6 @@ export class HomeContent extends React.Component<IHomeContentProps, IHomeContent
             console.error('!!! Error getting server times');
         });       
     }
-    private getAutoScalerJSON() {
-        this.session.GridAutoScaler.getJSON()
-        .then((autoScalerJSON: IGridAutoScalerJSON) => {
-            this.setState({autoScalerJSON})
-        }).catch((err: any) => {
-            console.error('!!! Error getting auto-scaler json');
-        });
-    }
-    private getAutoScalerImplementationInfo() {
-        this.session.GridAutoScaler.getImplementationInfo()
-        .then((autoScalerImplementationInfo: AutoScalerImplementationInfo) => {
-            this.setState({autoScalerImplementationInfo})
-        }).catch((err: any) => {
-            console.error('!!! Error getting auto-scaler config url');
-        });
-    }
     componentDidMount() {
         console.log('HomeContent.componentDidMount()');
         this.getServerTimes();
@@ -93,32 +73,12 @@ export class HomeContent extends React.Component<IHomeContentProps, IHomeContent
         }).catch((err: any) => {
             console.error('!!! Error: topic subscription failed');
         });
-        if (this.props.autoScalerAvailable) {
-            this.getAutoScalerJSON();
-            this.getAutoScalerImplementationInfo();
-            this.msgClient.subscribe(Utils.getAutoScalerTopic(), this.handleMessages.bind(this), {})
-            .then((autoscaler_sub_id: string) => {
-                console.log('autoscaler topic subscribed, sub_id=' + autoscaler_sub_id + " :-)");
-                this.setState({autoscaler_sub_id});
-            }).catch((err: any) => {
-                console.error('!!! Error: topic subscription failed');
-            });
-        }
     }
     componentWillUnmount() {
         console.log('HomeContent.componentWillUnmount()');
         if (this.state.timer) clearInterval(this.state.timer);
         if (this.state.disp_sub_id) {
             let sub_id = this.state.disp_sub_id;
-            this.msgClient.unsubscribe(sub_id)
-            .then(() => {
-                console.log('successfully unsubscribed subscription ' + sub_id);
-            }).catch((err: any) => {
-                console.error('!!! Error unsubscribing subscription ' + sub_id);
-            });
-        }
-        if (this.state.autoscaler_sub_id) {
-            let sub_id = this.state.autoscaler_sub_id;
             this.msgClient.unsubscribe(sub_id)
             .then(() => {
                 console.log('successfully unsubscribed subscription ' + sub_id);
@@ -221,40 +181,6 @@ export class HomeContent extends React.Component<IHomeContentProps, IHomeContent
         }        
     }
 
-    private get AutoScalerAvailable(): boolean {return this.props.autoScalerAvailable};
-    private get AutoScalerJSON(): IGridAutoScalerJSON {return this.state.autoScalerJSON;}
-    private get AutoScalerImplInfo(): AutoScalerImplementationInfo {return this.state.autoScalerImplementationInfo;}
-    // TODO: check profile
-    private get AllowToChangeAutoScalerConfig() : boolean {
-        if (!this.AutoScalerAvailable)
-            return false;
-        else if (!this.AutoScalerJSON)
-            return false;
-        else
-            return true;
-    }
-    private get AutoScalerEnabledText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.booleanString(this.AutoScalerJSON.Enabled) : null) : "N/A");}
-    private get AutoScalerScalingText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? (this.AutoScalerJSON.ScalingUp ? "Scaling up...": "Idle") : null) : "N/A");}
-    private get AutoScalerMaxNodesText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.MaxWorkersCap.toString() : null) : "N/A");}
-    private get AutoScalerMinNodesText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.MinWorkersCap.toString() : null) : "N/A");}
-    private get AutoScalerNodeLaunchingTimeoutText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.LaunchingTimeoutMinutes.toString() + " min." : null) : "N/A");}
-    private get AutoScalerTerminateWorkerAfterMinutesIdleText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.TerminateWorkerAfterMinutesIdle.toString() + " min." : null) : "N/A");}
-    private get AutoScalerRampUpSpeedRatioText() : string {return (this.AutoScalerAvailable ? (this.AutoScalerJSON ? this.AutoScalerJSON.RampUpSpeedRatio.toString() : null) : "N/A");}
-    private get AutoScalerImplName() : string {return (this.AutoScalerAvailable ? (this.AutoScalerImplInfo ? this.AutoScalerImplInfo.Name : null) : "N/A");}
-    private get HasAutoScalerImplSetupUI() : boolean {return (this.AutoScalerAvailable ? (this.AutoScalerImplInfo ? this.AutoScalerImplInfo.HasSetupUI : false) : false);}
-    private get AutoScalerImplSetupUILinkEnabled() : boolean {return this.AllowToChangeAutoScalerConfig && this.HasAutoScalerImplSetupUI;}
-    private get AutoScalerImplSetupUIUrl() : string {return (this.HasAutoScalerImplSetupUI ? "autoscaler/implementation" : "#");}
-
-    private onAutoScalerEnableClick(e:any) {
-        if (this.state.autoScalerJSON) {
-            let p = (this.state.autoScalerJSON.Enabled ? this.session.GridAutoScaler.disable() : this.session.GridAutoScaler.enable());
-            p.then(() => {
-
-            }).catch((err: any) => {
-                console.error('!!! Unable to enable/disable auto-scaler: ' + JSON.stringify(err));
-            });
-        }
-    }
     render() {
         return (
             <div>
@@ -333,69 +259,7 @@ export class HomeContent extends React.Component<IHomeContentProps, IHomeContent
                             </div>
                         </div>
 
-                        <div className="w3-card-4 w3-margin">
-                            <div className="w3-container w3-blue">
-                                <h6>Auto-Scaler ({this.AutoScalerAvailable ? "Available" : "N/A"})</h6>
-                            </div>
-                            <div className="w3-container w3-white">
-                                <table className="w3-table w3-bordered w3-small">
-                                    <thead>
-                                        <tr>
-                                            <th>Item</th>
-                                            <th>Value</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Enabled</td>
-                                            <td>{this.AutoScalerEnabledText}</td>
-                                            <td><button disabled={!this.AllowToChangeAutoScalerConfig} onClick={this.onAutoScalerEnableClick.bind(this)}>{!this.state.autoScalerJSON || this.state.autoScalerJSON.Enabled ? "Disable" : "Enable"}</button></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Scaling</td>
-                                            <td>{this.AutoScalerScalingText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Max. # of nodes</td>
-                                            <td>{this.AutoScalerMaxNodesText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Min. # of nodes</td>
-                                            <td>{this.AutoScalerMinNodesText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Node launching timeout</td>
-                                            <td>{this.AutoScalerNodeLaunchingTimeoutText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Terminate node after idle for </td>
-                                            <td>{this.AutoScalerTerminateWorkerAfterMinutesIdleText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ramp up speed ratio</td>
-                                            <td>{this.AutoScalerRampUpSpeedRatioText}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Implementation Name</td>
-                                            <td>{this.AutoScalerImplName}</td>
-                                            <td></td>
-                                        </tr>
-                                        <tr>
-                                            <td>Additional config.</td>
-                                            <td></td>
-                                            <td><a disabled={!this.AutoScalerImplSetupUILinkEnabled} href={this.AutoScalerImplSetupUIUrl}>Click Here</a></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                        <AutoScalerUI autoScalerAvailable={this.props.autoScalerAvailable} gridAutoScaler={this.session.GridAutoScaler} msgClient={this.msgClient}/>
                     </div>
                 </div>
             </div>
